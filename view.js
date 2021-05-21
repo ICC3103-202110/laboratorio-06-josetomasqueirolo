bce6356c8046950b207b51a3e21a1e433fdb23c6
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-05-21 17:14:46 -0400

    Haciendo función getTitle

---
+++ view.js
@@ -0,0 +1,17 @@
+const figlet = require('figlet')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+
+function getTitle(){
+    return chalk.yellow(
+        figlet.textSync(
+            'Unit Converter App',
+            {
+                horizontalLayout: 'full',
+                font: 'Nancyj-Underlined'
+            }
+        )
+    )
+}
+// para imprimir titulo hacer con console.log()
+
