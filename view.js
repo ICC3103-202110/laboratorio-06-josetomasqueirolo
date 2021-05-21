3b0ac23a66e61fd97130fc511fd84747753c7583
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-05-21 17:34:12 -0400

    Haciedo inputForm

--- view.js
+++ view.js
@@ -15,3 +15,32 @@ function getTitle(){
 }
 // para imprimir titulo hacer con console.log()
 
+function getTable(model){
+    const {leftValue} = model
+    const {leftUnit} = model
+    const {rightValue} = model
+    const {rightUnit} = model
+    return [
+        {"leftValue": leftValue, "leftUnit": leftUnit, "rightValue": rightValue, "rightUnit": rightUnit},
+    ]
+}
+
+function inputForm(model){
+    const {input} = model
+    const message = 'Left temperature is source (y/n)?'
+    return inquirer.prompt([
+        {
+            name: 'input',
+            type: 'input',
+            message: message,
+            default: input,
+            validate: function(value){
+                if(value === 'y' || value === 'n'){
+                    return true
+                } else {
+                    return "Enter 'y' or 'n'."
+                }
+            }
+        }
+    ])
+}
